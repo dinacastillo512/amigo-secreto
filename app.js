61368f67b047aabb4f473497d08b73b9d54806a5
dina castillo <85804060+dinacastillo512@users.noreply.github.com>
2025-02-09 15:52:33 -0300

    Update app.js
    Corrección de errores, arreglo de tabulaciones  en el código

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ let resultado=document.getElementById('resultado');
 function agregarAmigo(){
 //Captura el valor del campo de entrada Input del html correspondiente al nombre de un amigo.
 let valor =document.getElementById('amigo').value;
-
 if (amigos.length == 0){
       //Limpiar resultado porque se trata de un nuevo juego
       resultado.innerHTML = "";
 }
-
 //Valida la entrada.Si es igual a una cadena vacía o contiene números o caracteres distintos a letras ..
 if(valor=="" || soloLetrasConAcentos(valor)==false){
     alert("Por favor, ingrese un nombre válido");
@@ -30,24 +28,18 @@ if(valor=="" || soloLetrasConAcentos(valor)==false){
  document.getElementById('amigo').value="";
 }
 
-
 function actualizarLista(){ 
-   
     // Iteramos sobre los datos y creamos elementos li
     amigos.forEach(amigo => {
     const li = document.createElement('li');
     li.textContent = amigo;
     lista.appendChild(li);    
-    });
-   
+    }); 
 }
+
 //Expresión regular que evalúa una cadena y retorna verdadero si solo contiene letras
 function soloLetrasConAcentos(cadena) {
     return /^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ\s]+$/.test(cadena);
-}
-function reiniciarJuego(){
-  
-    
 }
 
 //elige un amigo de entre los ingresados
@@ -57,10 +49,9 @@ function sortearAmigo(){
         let amigoSorteado = Math.floor(Math.random()*amigos.length);
         lista.innerHTML="";
         resultado.innerHTML='El amigo secreto sorteado es ' + amigos[amigoSorteado];
-        
     }else{
         alert("Debes ingresar como mínimo un amigo");
     }
-   //vaciar el array
+   //vaciar el array para un nuevo juego
    amigos=[];
 }
